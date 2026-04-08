f1a57873d434a18766e035e1ce5d5f140db60fd6
Samuel Schlesinger <sgschlesinger@gmail.com>
2026-04-08 10:24:58 -0400

    added some documentation, upgraded toolchain

--- Cat/Theory.lean
+++ Cat/Theory.lean
@@ -1,5 +1,60 @@
+/-
+Copyright (c) 2026 Samuel G. Schlesinger. All rights reserved.
+Authors: Samuel G. Schlesinger
+-/
 namespace Cat.Theory
 
+/-!
+
+# Category Theory
+
+This file is a self-contained implementation of category theory implemented for
+my own education, and perhaps of some readers though that is much less obvious.
+
+A category is a natural mathematical object originally penned by Saunders and
+Mac Lane in their 1945 paper "General Theory of Natural Equivalences". At a
+birds-eye view, a category is anything with a transitive, reflexive law of
+composition. This might sound like a monoid, and it should, but it provides an
+extra layer of precision (or "type-safety" by another framing) enabling the
+category to not permit every element to compose.
+
+NB: This file was written by hand, without the help of AI. No contributions
+will be accepted, it is a personal project.
+
+-/
+
+/--
+
+  A category consists of a set of objects, a set of morphisms parameterized by
+  those objects, a law of composition which tells you how to compose those
+  morphisms, and, for every object, an identity morphism associated with that
+  object.
+
+  There are two laws:
+  - associativity: the composition law must not depend on the nesting of
+    composition, though order indeed does matter.
+  - identity: the identity morphism must be the identity under composition.
+
+  In category theory, we often express things in the form of diagrams. One way
+  of viewing this definition as the following, so-called commutative diagram.
+
+              f
+          x -----> y
+           \       |
+            \      |
+             \     | g
+  compose f g \    |
+               \   v
+                >  z
+
+  What we mean when we say a diagram is commutative is that, whichever order you
+  take through the diagram, the resulting composition is the same.
+
+  The identity diagram is harder to draw in ASCII because it has a loop, but you
+  hopefully get the idea. Either way, read through the mathematical definitions
+  to understand better.
+
+-/
 @[ext]
 structure Category where
   obj : Type u
@@ -12,6 +67,12 @@ structure Category where
   identity :
     compose f id = f ∧ compose id f = f
 
+/--
+
+  The opposite category is simply a flipping of the convention from left to
+  right composition.
+  
+-/
 @[simp]
 def Category.op (c : Category) : Category where
   obj := c.obj
@@ -27,10 +88,22 @@ def Category.op (c : Category) : Category where
     . rw [c.identity.2]
     . rw [c.identity.1]
 
+/-- 
+
+  A morphism is said to be an isomorphism if it has a complementary morphism
+  such that composing them together (in either order) results in the identity
+  morphism.
+
+-/
 structure Category.Isomorphism { c : Category } { x y : c.obj } (forward : c.hom x y) where
   backward : c.hom y x
   is_inverse : c.compose forward backward = c.id ∧ c.compose backward forward = c.id
 
+/--
+
+  The inverses witnessing isomorphisms are unique.
+
+-/
 theorem Category.Isomorphism.unique { c : Category } { x y : c.obj }
   (f : c.hom x y) (iso₀ : Isomorphism f) (iso₁ : Isomorphism f) :
     iso₀.backward = iso₁.backward := by
@@ -47,6 +120,12 @@ theorem Category.Isomorphism.unique { c : Category } { x y : c.obj }
           rw [h0]
         _  = iso₁.backward := c.identity.1
 
+/--
+
+  Isomorphisms can be flipped and, because the laws are symmetric, are still
+  isomorphisms.
+
+-/
 def Category.Isomorphism.sym { c : Category } { x y : c.obj } { f : c.hom x y }
   (iso₀ : Isomorphism f) : Isomorphism iso₀.backward := {
     backward := f
@@ -56,6 +135,12 @@ def Category.Isomorphism.sym { c : Category } { x y : c.obj } { f : c.hom x y }
       . exact iso₀.is_inverse.1
   }
 
+/--
+
+  Our first real category. Here, objects are types and morphisms are functions
+  between those types.
+
+-/
 @[simp]
 def Category.set : Category where
   obj := Type
@@ -71,6 +156,14 @@ def Category.set : Category where
     . rfl
     . rfl
 
+/--
+
+  The product of an arbitrary set of categories, indexed by a type α, is also a
+  category. Objects are products of objects, morphisms are products of
+  morphisms. Composition is parallel composition, identity is parallel
+  identity.
+
+-/
 @[simp]
 def Category.gen_product { α : Type u } (categories : α → Category) : Category where
   obj := (i : α) → (categories i).obj
@@ -90,6 +183,13 @@ def Category.gen_product { α : Type u } (categories : α → Category) : Catego
     . funext i
       rw [(categories i).identity.2]
 
+/--
+
+  A specialized version of the product category.
+
+  Can be seen to be isomorphic to the gen_product with |α| = 2.
+
+-/
 @[simp]
 def Category.product (c : Category) (d : Category) : Category where
   obj := c.obj × d.obj
@@ -109,6 +209,16 @@ def Category.product (c : Category) (d : Category) : Category where
     . rw [c.identity.1, d.identity.1]
     . rw [c.identity.2, d.identity.2]
 
+/--
+
+  A functor is a structure preserving map between categories. It contains a way
+  to map objects to objects and arrows to arrows.
+
+  It has two laws:
+  - homomorphic: the arrow map must preserve composition.
+  - identity: the arrow map must preserve identities.
+
+-/
 @[ext]
 structure Functor (c : Category) (d : Category) where
   obj : c.obj → d.obj
@@ -120,6 +230,12 @@ structure Functor (c : Category) (d : Category) where
     ∀ (x : c.obj),
       arr (c.id (x := x)) = d.id (x := obj x)
 
+/--
+
+  The identity functor simply maps objects to themselves and arrows to
+  themselves. Of course this preserves structure
+
+-/
 @[simp]
 def Functor.id (c : Category) : Functor c c where
   obj x := x
@@ -131,6 +247,12 @@ def Functor.id (c : Category) : Functor c c where
     intros x
     rfl
 
+/--
+
+  The category of categories. Our second category, here the objects are
+  categories and morphisms are functors between categories.
+
+-/
 @[simp]
 def Category.cat : Category where
   obj := Category
@@ -168,6 +290,14 @@ def Category.cat : Category where
       . simp
         rfl
 
+/--
+
+  The hom functor is a functor from C^op × C to Set. It maps pairs of objects
+  to the hom set of C. It maps pairs of arrows to a function which performs
+  simultaneous pre- and post-composition. In my head-canon thus, this is the
+  sandwich functor.
+
+-/
 def Functor.hom (c : Category) : Functor (c.op.product c) Category.set where
   obj x := c.hom x.1 x.2
   arr f := λ g ↦ c.compose (c.compose f.1 g) f.2
@@ -182,6 +312,18 @@ def Functor.hom (c : Category) : Functor (c.op.product c) Category.set where
     simp
     simp [c.identity]
 
+/--
+
+  A monad for a functor f contains essentially the equipment for composing
+  arrows a → f b with arrows b → f c such that the resulting composition
+  law forms a category, the Kleisli category. As such, it must also contain
+  the identity in that category.
+
+  In functional programming languages, these make a particularly appealing
+  abstraction for certain types of control flow, and so many of us are familiar
+  with them.
+
+-/
 structure Monad (f : Functor c c) where
   bind : c.hom a (f.obj b) → c.hom (f.obj a) (f.obj b)
   pure : c.hom a (f.obj a)
@@ -192,17 +334,29 @@ structure Monad (f : Functor c c) where
     ∀ (s : c.hom α (f.obj β)) (t : c.hom β (f.obj χ)),
       c.compose (bind s) (bind t) = bind (c.compose s (bind t))
 
+/--
+
+  The identity functor is a monad in a rather uninteresting way.
+
+-/
 def Monad.id : Monad (Functor.id c) where
   bind := λ x ↦ x
   pure := c.id
   left_unit := rfl
   right_unit := by
     intros
-    rw [c.identity.2]
+    simp [c.identity.2]
   associativity := by
     intros α β χ s t
     rfl
 
+/--
+
+  Every monad defines a category via Kleisli composition. Monads are basically
+  defined such that this category exists, as we discussed before, and this
+  category is what makes monads composable.
+
+-/
 @[simp]
 def Category.kleisli (m : Functor c c) (monad : Monad m) : Category where
   obj := c.obj
@@ -218,6 +372,12 @@ def Category.kleisli (m : Functor c c) (monad : Monad m) : Category where
     . rw [monad.left_unit, c.identity.1]
     . rw [monad.right_unit]
 
+/--
+
+  Comonads are the co-thing to a monad. In functional programming, these make
+  an appearance, but less so.
+
+-/
 structure Comonad (f : Functor c c) where
   extend : c.hom (f.obj a) b → c.hom (f.obj a) (f.obj b)
   extract : c.hom (f.obj a) a
@@ -228,17 +388,27 @@ structure Comonad (f : Functor c c) where
     ∀ (s : c.hom (f.obj α) β) (t : c.hom (f.obj β) χ),
       c.compose (extend s) (extend t) = extend (c.compose (extend s) t)
 
+/--
+
+  The identity functor, again, is a comonad.
+
+-/
 def Comonad.id : Comonad (Functor.id c) where
   extend := λ x ↦ x
   extract := c.id
   left_unit := rfl
   right_unit := by
     intros
-    rw [c.identity.1]
+    simp [c.identity.1]
   associativity := by
     intros α β χ s t
     rfl
 
+/--
+
+  And you wouldn't believe it, there is a category here too.
+
+-/
 @[simp]
 def Category.cokleisli (w : Functor c c) (comonad : Comonad w) : Category where
   obj := c.obj
@@ -254,6 +424,28 @@ def Category.cokleisli (w : Functor c c) (comonad : Comonad w) : Category where
     . rw [comonad.right_unit]
     . rw [comonad.left_unit, c.identity.2]
 
+/--
+
+  Now we get to the meat and potatoes of category theory. A natural transformation
+  between two functors is really why Saunders and Mac Lane defined this stuff to
+  begin with. They wanted to understand what about the isomoprhism between V and
+  V** was more "natural" than the isomorphisms between V and V*, which rely on a
+  choice of basis.
+
+  Essentially, a natural transformation between f and g, both functors from c to d,
+  is a morphism for every object x, η x, such that η x f-g-commutes. In particular,
+  this diagram commutes
+
+                η x
+    f.obj x -------------> g.obj x
+      |                      |
+      | f.arr f              | g.arr f
+      |                      |
+      v                      v
+    f.obj y -------------> g.obj y
+                η y
+
+-/
 @[ext]
 structure NaturalTransformation (f : Functor c d) (g : Functor c d) where
   η : ∀ (x : c.obj), d.hom (f.obj x) (g.obj x)
@@ -261,6 +453,13 @@ structure NaturalTransformation (f : Functor c d) (g : Functor c d) where
     ∀ (r : c.hom x y),
       d.compose (η x) (g.arr r) = d.compose (f.arr r) (η y)
 
+/--
+
+  The category of functors between two specific categories. The objects are the
+  functors between those categories, and the morphisms are natural
+  transformations between them.
+
+-/
 def Category.functor (c : Category) (d : Category) : Category where
   obj := Functor c d
   hom f g := NaturalTransformation f g
@@ -293,6 +492,12 @@ def Category.functor (c : Category) (d : Category) : Category where
     . simp [d.identity]
     . simp [d.identity]
 
+/--
+
+  A natural transformation is said to be a natural isomoprhism if all of its η
+  x are isomorphisms.
+
+-/
 @[ext]
 structure NaturalIsomorphism (f : Functor c d) (g : Functor c d) where
   nt : NaturalTransformation f g
